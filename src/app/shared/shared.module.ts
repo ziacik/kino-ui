fdafe2671e56c19a06ee4599a567a04cf13eb158
František Žiačik <ziacik@gmail.com>
2017-11-06 22:27:59 +0100

    Angular upgrade to 5, material upgrade, masonry removed.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,37 +1,41 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MasonryModule } from 'angular2-masonry';
 import { ItemsGridComponent } from './items-grid/items-grid.component';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { ItemService } from './item.service';
 import { HttpClientModule } from "@angular/common/http";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule, MatCardModule, MatButtonModule, MatInputModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		BrowserModule,
-		MaterialModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
-		MasonryModule
+		FlexLayoutModule,
+		MatSidenavModule,
+		MatCardModule,
+		MatButtonModule,
+		MatInputModule,
+		MatToolbarModule,
+		MatIconModule
 	],
 	exports: [
 		CommonModule,
 		BrowserModule,
-		MaterialModule,
 		BrowserAnimationsModule,
-		MasonryModule,
+		FlexLayoutModule,
 		ItemsGridComponent,
-		ItemCardComponent
+		ItemCardComponent,
+		MatSidenavModule,
+		MatCardModule,
+		MatButtonModule,
+		MatInputModule,
+		MatToolbarModule,
+		MatIconModule
 	],
 	declarations: [
 		ItemsGridComponent,
